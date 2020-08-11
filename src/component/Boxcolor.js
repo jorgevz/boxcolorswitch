0b2e660949d3339121a296c49033492c10340a89
jorgevz <jorgev2132@gmail.com>
2020-08-11 18:21:54 -0400

    setting my states

--- src/component/Boxcolor.js
+++ src/component/Boxcolor.js
@@ -7,7 +7,7 @@ import ReactDOM from 'react-dom';
 const blue = "#10B5B5";
 const beige = "#E6D9BC";
 
-//Here i have both of my states set for both colors.
+//Here i have both of my states set for both colors
 
 export default class Toggle extends React.Component{
   constructor(props){
